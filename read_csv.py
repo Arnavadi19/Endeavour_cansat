df9851818a9ac53c019273ce8937366d7e90d1b4
Arnav Aditya <arnavdt@gmail.com>
2024-02-02 19:14:18 +0530

    added software architecture

--- read_csv.py
+++ read_csv.py
@@ -1,2 +1,7 @@
 #read pressure sim values and store it .create telemetry string and transfer it via xbee to fsw
+#example string: CMD,2006,SIMP,XYZ​
+import matplotlib.pyplot as plt
+import pandas as pd
+
+provided_csv = pd.read_csv('sample.csv')
 
